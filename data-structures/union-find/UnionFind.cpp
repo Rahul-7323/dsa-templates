0d5f562679664e15f5ab7357bb68d5ed630ee9f1
Rahul M <roboto7o32oo3@gmail.com>
2023-05-13 13:04:42 +0530

    Update UnionFind.cpp, simplify the implementation

--- data-structures/union-find/UnionFind.cpp
+++ data-structures/union-find/UnionFind.cpp
@@ -9,75 +9,30 @@
  */
 
 class UnionFind {
-
 public:
-
-    vector<int> parent, rank, size;
-
+    vector<int> parent, size;
+    
     UnionFind(int n) {
-        parent.resize(n, -1);
-        rank.resize(n, 0);
-        size.resize(n, 1);
+        parent.assign(n, -1);
+        size.assign(n, 1);
     }
-
-    // finds the parent of given node u
+    
     int find(int u) {
-        // if the parent of the node is -1, then it is the root of the set 
         if(parent[u] == -1) return u;
-        // else, find the parent while also compressing the paths
         return parent[u] = find(parent[u]);
     }
-
-    // merges two sets into one, u and v belong to the corresponding sets
-    // performs union by rank
-    bool unionByRank(int u, int v) {
-        // find the parents of u and v
+    
+    bool unite(int u, int v) {
         u = find(u);
         v = find(v);
-
-        // if u and v does not belong to the same set
-        if(u != v) {
-            // if tree has lower rank
-            if(rank[u] < rank[v]) {
-                swap(u, v);
-            }
-
-            // attach lower rank tree to higher rank tree
-            parent[v] = u;
-
-            // if the ranks become equal, then increase the rank
-            if(rank[u] == rank[v]) {
-                rank[u]++;
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    // perform union by size
-    bool unionBySize(int u, int v) {
-        // find the parents of u and v
-        u = find(u);
-        v = find(v);
-
-        // if u and v do not belong to the same set
-        if(u != v) {
-            // if size of u is smaller
-            if(size[u] < size[v]) {
-                swap(u, v);
-            }
-
-            // attach smaller size tree to larger size tree
-            parent[v] = u;
-
-            // the tree u now contains all the elements of v as well
-            size[u] += size[v];
-
-            return true;
-        }
-
-        return false;
+        
+        if(u == v) return false;
+        
+        if(size[u] < size[v]) swap(u, v);
+        
+        parent[v] = u;
+        size[u] += size[v];
+        
+        return true;
     }
 };
