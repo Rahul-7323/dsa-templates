db4ea4050b5f322eecbe1c9e608d6a7150ee35a7
Rahul M <roboto7o32oo3@gmail.com>
2023-05-20 17:25:56 +0530

    added longest common substring

---
+++ algorithms/dynamic-programming/longest-common-substring/lcs.cpp
@@ -0,0 +1,19 @@
+
+int lcs(string& s1, string& s2) {
+    int m = s1.size(), n = s2.size();
+
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    int result = 0;
+
+    for(int i=1; i<=m; i++) {
+        for(int j=1; j<=n; j++) {
+            if(s1[i-1] == s2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+
+            result = max(result, dp[i][j]);
+        }
+    }
+
+    return result;
+}
